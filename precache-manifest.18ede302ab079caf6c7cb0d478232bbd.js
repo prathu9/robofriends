a69b607e64d9f4b9423f6a326d3fca0ac1c03aab
Prathamesh Sawant <prathameshpgs@gmail.com>
2020-09-21 21:56:23 +0530

    Updates

---
+++ precache-manifest.18ede302ab079caf6c7cb0d478232bbd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf5c5de5926da8f6b1255f341915287c",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "0c04b8e56745652525a8",
+    "url": "/robofriends/static/css/main.227d4925.chunk.css"
+  },
+  {
+    "revision": "e13aa5aa07d8cdbcc425",
+    "url": "/robofriends/static/js/2.5e776654.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.5e776654.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c04b8e56745652525a8",
+    "url": "/robofriends/static/js/main.b451a8c2.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
